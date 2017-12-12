421ff4edc113246d1a19efc4fec9fcd616390d61
Saul Femm <saulfemm@outlook.com>
2017-12-12 16:43:52 -0600

    Create Basic note type if it doesn't exist

--- __init__.py
+++ __init__.py
@@ -6,23 +6,54 @@ import subprocess as sp
 from aqt import mw
 from aqt.qt import *
 from aqt.utils import showInfo
-from anki import utils
+from anki import utils, sound
 
-class App(QApplication):
-    def __init__(self, args):
-        super().__init__(args)
-
-        self.mpv = 'mpv'
-        if utils.isWin:
-            self.mpv = os.path.join(os.path.dirname(sys.executable), 'mpv.com')
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.check_mpv()
+        self.set_model()
+        
+        self.inputPath = None
 
         self.buildGUI()
 
+        # Simulate file select button push
+        self.fileSelectBtnPushed()
+
+    def check_mpv(self):
+        self.mpv = 'mpv'
+        if utils.isWin:
+            self.mpv = os.path.join(os.path.dirname(sound.mpvPath[0]), 'mpv.com')
+        mplayer_test = utils.call([self.mpv, '-V', '--no-terminal'])
+        if mplayer_test == -1:
+            showInfo('MPV not found.')
+            self.close()
+
+    def set_model(self):
+        # Check for 'Basic' note type, create it if it doesn't exist
+        basicModel = mw.col.models.byName('Basic')
+        if not basicModel:
+            basicModel = mw.col.models.new('Basic')
+
+            basicTemplate = mw.col.models.newTemplate('Card 1')
+            basicTemplate['qfmt'] = '{{Front}}'
+            basicTemplate['afmt'] = '{{FrontSide}}\n\n<hr id="answer">\n\n{{Back}}'
+            mw.col.models.addTemplate(basicModel, basicTemplate)
+
+            frontField = mw.col.models.newField('Front')
+            mw.col.models.addField(basicModel, frontField)
+
+            backField = mw.col.models.newField('Back')
+            mw.col.models.addField(basicModel, backField)
+
+            mw.col.models.add(basicModel)
+        mw.col.models.setCurrent(basicModel)
+
     def buildGUI(self):
         # Create window and layout
-        self.window = QWidget()
         self.layout = QGridLayout()
-        self.window.setLayout(self.layout)
+        self.setLayout(self.layout)
 
         # Create widgets
         self.intervalLbl = QLabel("Seconds between frames: ")
@@ -38,14 +69,6 @@ class App(QApplication):
         self.totalFramesSpin.valueChanged.connect(self.totalFramesSpinChanged)
         self.commenceBtn.pressed.connect(self.commenceBtnPushed)
 
-        # Add toggle widgets to list
-        self.toggleWidgets = [
-            self.intervalSpin, self.totalFramesSpin,
-            self.fileSelectBtn, self.commenceBtn]
-
-        # Disable toggle widgets
-        self.setToggleWidgets(False)
-
         # Add widgets to layout
         self.layout.addWidget(self.intervalLbl, 0, 0)
         self.layout.addWidget(self.intervalSpin, 0, 1)
@@ -54,22 +77,12 @@ class App(QApplication):
         self.layout.addWidget(self.fileSelectBtn, 2, 0)
         self.layout.addWidget(self.commenceBtn, 2, 1)
 
-        # Simulate file select button push
-        self.fileSelectBtnPushed()
-
-        # Display app
-        self.window.show()
-
-    def setToggleWidgets(self, state):
-        for widget in self.toggleWidgets:
-            widget.setEnabled(state)
-
     def fileSelectBtnPushed(self):
         # Open file dialog to choose video
         self.inputPath = \
             QFileDialog.getOpenFileName(
-                    caption='Select Video File',
-                    filter='Video Files (*.avi *.flv *.mkv *.mp4 *.mpg *.wmv)')[0]
+                caption='Select Video File',
+                filter='Video Files (*.avi *.flv *.mkv *.mp4 *.mpg *.wmv)')[0]
 
         # Use mpv to get video length
         command = [
@@ -78,15 +91,14 @@ class App(QApplication):
             '--vo=null', '--ao=null',
             '--frames=1', '--no-cache', '--no-config',
             '--quiet', self.inputPath]
-        results = sp.check_output(command).decode('utf-8')
+        startupinfo = sp.STARTUPINFO()
+        startupinfo.dwFlags |= sp.STARTF_USESHOWWINDOW
+        results = sp.check_output(command, startupinfo=startupinfo).decode('utf-8')
         self.videoLength = int(re.search(r'LENGTH=(\d*)', results).groups()[0])
 
         # Move spinbox ranges
         self.intervalSpin.setRange(1, self.videoLength)
         self.totalFramesSpin.setRange(1, self.videoLength)
-
-        # Enable widgets
-        self.setToggleWidgets(True)
 
     # Link values of delay and total shots
     def intervalSpinChanged(self):
@@ -99,6 +111,9 @@ class App(QApplication):
         self.intervalSpin.setValue(interval)
 
     def commenceBtnPushed(self):
+        if not self.inputPath:
+            return
+
         baseName = os.path.basename(self.inputPath)
         fileName, fileExtension = os.path.splitext(baseName)
 
@@ -106,42 +121,26 @@ class App(QApplication):
         command = [
             self.mpv, '--vo=image', f'--vo-image-outdir={tmpDir}',
             '-sstep', str(self.intervalSpin.value()), self.inputPath]
-        self.setToggleWidgets(False)
-        self.processEvents()
+        self.setEnabled(False)
+        mw.app.processEvents()
         utils.call(command)
 
         for frame in os.listdir(tmpDir):
             newPath = os.path.join(mw.col.media.dir(), f'{fileName}_{frame}')
             shutil.move(os.path.join(tmpDir, frame), newPath)
+
             note = mw.col.newNote()
             note['Front'] = f'<img src=\"{fileName}_{frame}\">'
             note['Back'] = fileName
+            note.tags.append('videotoframes')
             mw.col.addNote(note)
 
-        self.setToggleWidgets(True)
+        self.setEnabled(True)
 
 def start():
-    # Check that mpv is available
-    mpv = 'mpv'
-    if utils.isWin:
-        mpv = os.path.dirname(sys.executable) + '\mpv.com'
-
-    mplayer_test = utils.call([mpv, '-V', '--really-quiet'])
-    if mplayer_test == -1:
-        showInfo('MPV not found.')
-        return
-
-    # Check for 'Basic' note type
-    # TODO Create Basic note type if it doesn't exist
-    basicModel = mw.col.models.byName('Basic')
-    if not basicModel:
-        showInfo('No Basic note type')
-        return
-    mw.col.models.setCurrent(basicModel)
-
-    mw.myApp = app = App(sys.argv)
+    mw.myWidget = widget = App()
+    widget.show()
 
 action = QAction('VideoToFrames', mw)
 action.triggered.connect(start)
 mw.form.menuTools.addAction(action)
-
